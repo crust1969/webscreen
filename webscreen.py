09c7467ce5606d83ec2509e1e3491f8b832c3e21
crust1969 <59149242+crust1969@users.noreply.github.com>
2024-07-12 14:24:39 +0200

    Update webscreen.py

--- webscreen.py
+++ webscreen.py
@@ -1,6 +1,36 @@
 import streamlit as st
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+# Spotify credentials
+SPOTIPY_CLIENT_ID = 'your_spotify_client_id'
+SPOTIPY_CLIENT_SECRET = 'your_spotify_client_secret'
+SPOTIPY_REDIRECT_URI = 'your_spotify_redirect_uri'
+
+# Google credentials
+GOOGLE_CLIENT_SECRETS_FILE = 'client_secret.json'
+SCOPES = ['https://www.googleapis.com/auth/youtube.readonly']
 
 st.set_page_config(layout="wide")
+
+st.sidebar.title("Login")
+
+# Spotify authentication
+if 'spotify_token' not in st.session_state:
+    sp_oauth = SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID,
+                            client_secret=SPOTIPY_CLIENT_SECRET,
+                            redirect_uri=SPOTIPY_REDIRECT_URI,
+                            scope='user-library-read')
+    token_info = sp_oauth.get_access_token()
+    st.session_state['spotify_token'] = token_info['access_token']
+
+# Google authentication
+if 'google_token' not in st.session_state:
+    flow = InstalledAppFlow.from_client_secrets_file(GOOGLE_CLIENT_SECRETS_FILE, SCOPES)
+    credentials = flow.run_local_server(port=0)
+    st.session_state['google_token'] = credentials.token
 
 # Spotify embed
 spotify_url = "https://open.spotify.com/embed/playlist/37i9dQZF1DXcBWIGoYBM5M"
